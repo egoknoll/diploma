e2e3bdd5670104e57eed52408fef6038e5d066eb
Egor <egoknoll@gmail.com>
2023-04-25 16:11:05 +0300

    add fix

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -13,7 +13,7 @@ export const useAppMediaQuery = () => {
     useEffect(() => {
         window.addEventListener('resize', handleResize)
         return () => window.removeEventListener('resize', handleResize)
-    })
+    }, [windowSize])
 
     return windowSize
 }
